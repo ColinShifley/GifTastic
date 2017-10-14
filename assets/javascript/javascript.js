c5984f2d703c5809d2d9584cfdb30041e078ffcc
Colin Shifley <colin@Colins-MacBook-Pro.local>
2017-10-13 21:55:56 -0400

    final update

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -29,6 +29,7 @@ function giphyCall() {
             var a = $("<button>");
             // Adding a class of movie to our button
             a.addClass("gamey");
+
             // Adding a data-attribute
             a.attr("data-name", games[i]);
             theGame = ("data-name");
@@ -44,130 +45,104 @@ function giphyCall() {
     }
     renderButtons();
 
-function displayGameInfo() {
-    $("button").on("click", function() {
-        event.stopPropagation();
-        // In this case, the "this" keyword refers to the button that was clicked
-        var game = $(this).attr(theGame)
+    //function to to show Gifs and Rating
+    function displayGameInfo() {
+        $("button").on("click", function() {
 
-        // Constructing a URL to search Giphy for the name of the person who said the quote
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            game + "&api_key=dc6zaTOxFJmzC&limit=10";
+            // In this case, the "this" keyword refers to the button that was clicked
+            var game = $(this).attr(theGame)
+            
+            // Constructing a URL to search Giphy for the game
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                game + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-        // Performing our AJAX GET request
-        $.ajax({
-                url: queryURL,
-                method: "GET"
-            })
-            // After the data comes back from the API
-            .done(function(response) {
-                // Storing an array of results in the results variable
-                var results = response.data;
-                console.log(response)
-                // Looping over every result item
-                for (var i = 0; i < results.length; i++) {
+            // Performing our AJAX GET request
+            $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                })
+                // After the data comes back from the API
+                .done(function(response) {
+                    // Storing an array of results in the results variable
+                    var results = response.data;
+                    console.log(response)
+                    // Looping over every result item
+                    for (var i = 0; i < results.length; i++) {
 
-                    // Only taking action if the photo has an appropriate rating
-                    //if (results[i].rating !== "g" && results[i].rating !== "pg-13") {
-                    // Creating a div with the class "item"
-                    gifDiv = $("<div>");
-                    
-                    var im = results[i].images;
+                        // Only taking action if the photo has an appropriate rating
+                        //if (results[i].rating !== "g" && results[i].rating !== "pg-13") {
+                        // Creating a div with the class "item"
+                        gifDiv = $("<div>");
+                        gifDiv.addClass("gifness")
 
-                    // Storing the result item's rating
-                    var rating = results[i].rating;
+                        var im = results[i].images;
 
-                    // Creating a paragraph tag with the result item's rating
-                    var p = $("<p>").text("Rating: " + rating);
+                        // Storing the result item's rating
+                        var rating = results[i].rating;
 
-                    // Creating an image tag
-                    personImage = $("<img>");
-                    personImage.addClass("still");
-                    // Giving the image tag an src attribute of a proprty pulled off the
-                    // result item
-                    personImage.attr({"src" : im.fixed_width_still.url, "data-url": im.fixed_width.url});
+                        // Creating a paragraph tag with the result item's rating
+                        var p = $("<p>").text("Rating: " + rating);
+                        p.addClass("words")
+                        // Creating an image tag
+                        personImage = $("<img>");
+                        personImage.addClass("still");
+                        // Giving the image tag an src attribute of a proprty pulled off the
+                        // result item
+                        personImage.attr({ "src": im.fixed_width_still.url, "data-url": im.fixed_width.url });
 
-                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
-                    gifDiv.append(p);
-                    gifDiv.append(personImage);
-
-                    
-                        //event.stopPropagation();
-                        $(document).on("click", ".still", function() { 
-                        var tempImage = $(this).attr("src");
-                            $(this).attr("src", $(this).attr("data-url"));
-                            $(this).attr("data-url", tempImage);
+                        // Appending the paragraph and personImage we created to the "gifDiv" div we created
                         
-                        
-                        //$(this).append(personImage);
-                    });
+                        gifDiv.append(personImage);
+                        gifDiv.append(p);
 
-                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                    $("#gifs-appear-here").prepend(gifDiv);
 
-                    
-                    //}
-                }
-            })
+
+
+
+                        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                        $("#gifs-appear-here").prepend(gifDiv);
+
+
+                        //}
+                    }
+                })
         })
     };
 
-    	$("#add-game").on("click", function(event) {
+    //function to input new game to the button area at top
+    $("#add-game").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
         var gameInput = $("#game-input").val().trim();
 
-        // Adding movie from the textbox to our array
+        // Adding game from the textbox to our array
         games.push(gameInput);
 
-        // Calling renderButtons which handles the processing of our movie array
+        // Calling renderButtons which handles the processing of our games array
         renderButtons();
-      });
+    });
 
-    	$(document).on("click", ".gamey", displayGameInfo);
+    $(document).on("click", ".gamey", displayGameInfo);
 
-renderButtons()
+    renderButtons()
 }
 giphyCall();
 
 
-
-// Function for displaying movie data
-
-/*
-                
-
-
-
+$(document).on("click", ".still", function() {
+    
+    var tempImage = $(this).attr("src");
+    $(this).attr("src", $(this).attr("data-url"));
+    console.log(this)
+    $(this).attr("data-url", tempImage);
 
 
 
+});
 
 
 
-                // Creating and storing a div tag
-                var gameDiv = $("<div>");
-
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + results[i].rating);
-
-                // Creating and storing an image tag
-                var gameImage = $("<img>");
-                // Setting the src attribute of the image to a property pulled off the result item
-                gameImage.attr("src", results[i].images.fixed_height.url);
-
-                // Appending the paragraph and image tag to the animalDiv
-                gameDiv.append(p);
-                gameDiv.append(gameImage);
-
-                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-                $("#gifs-appear-here").prepend(gameDiv);
-            }*/
 
 
 
-//function to to show Gifs and Rating
 
-//function to input new game to the button area at top
-
-//
